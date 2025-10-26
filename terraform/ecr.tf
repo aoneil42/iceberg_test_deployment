587ae32a14f15565a39ab1f53ffe7274b08532cd
Austin Stevens <geoaustin@gmail.com>
2025-10-26 15:11:40 -0600

    updating tf ecr

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,5 @@
+# ECR Repositories for Docker images
+
 resource "aws_ecr_repository" "polaris" {
   name                 = "polaris-catalog"
   image_tag_mutability = "MUTABLE"
@@ -6,78 +8,74 @@ resource "aws_ecr_repository" "polaris" {
     scan_on_push = true
   }
 
-  encryption_configuration {
-    encryption_type = "AES256"
+  tags = {
+    Name        = "Polaris Catalog"
+    Project     = "Iceberg Geospatial Platform"
+    Environment = var.environment
   }
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "polaris-catalog"
-    }
-  )
 }
 
 resource "aws_ecr_repository" "ogc_api" {
-  name                 = "ogc-api-features"
+  name                 = "ogc-api"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
 
-  encryption_configuration {
-    encryption_type = "AES256"
+  tags = {
+    Name        = "OGC API Features"
+    Project     = "Iceberg Geospatial Platform"
+    Environment = var.environment
   }
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "ogc-api-features"
-    }
-  )
 }
 
-# Lifecycle policy for Polaris repository
+# Lifecycle policy to keep only last 10 images
 resource "aws_ecr_lifecycle_policy" "polaris" {
   repository = aws_ecr_repository.polaris.name
 
   policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1
-        description  = "Keep last 5 images"
-        selection = {
-          tagStatus   = "any"
-          countType   = "imageCountMoreThan"
-          countNumber = 5
-        }
-        action = {
-          type = "expire"
-        }
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 10 images"
+      selection = {
+        tagStatus     = "any"
+        countType     = "imageCountMoreThan"
+        countNumber   = 10
       }
-    ]
+      action = {
+        type = "expire"
+      }
+    }]
   })
 }
 
-# Lifecycle policy for OGC API repository
 resource "aws_ecr_lifecycle_policy" "ogc_api" {
   repository = aws_ecr_repository.ogc_api.name
 
   policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1
-        description  = "Keep last 5 images"
-        selection = {
-          tagStatus   = "any"
-          countType   = "imageCountMoreThan"
-          countNumber = 5
-        }
-        action = {
-          type = "expire"
-        }
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 10 images"
+      selection = {
+        tagStatus     = "any"
+        countType     = "imageCountMoreThan"
+        countNumber   = 10
       }
-    ]
+      action = {
+        type = "expire"
+      }
+    }]
   })
+}
+
+# Outputs
+output "polaris_ecr_url" {
+  description = "ECR repository URL for Polaris"
+  value       = aws_ecr_repository.polaris.repository_url
+}
+
+output "ogc_api_ecr_url" {
+  description = "ECR repository URL for OGC API"
+  value       = aws_ecr_repository.ogc_api.repository_url
 }
