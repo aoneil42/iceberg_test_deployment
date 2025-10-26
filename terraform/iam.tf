9a60da0ac4130a94c0e30d22e06b68c4c70d6ab4
Austin Stevens <geoaustin@gmail.com>
2025-10-26 15:55:46 -0600

    Switch to SSM for EC2 deployments

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -144,3 +144,33 @@ resource "aws_iam_role_policy_attachment" "ssm_managed_instance" {
   role       = aws_iam_role.geospatial_platform.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
+
+# SSM Policy for EC2 instance
+resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Additional SSM policy for session manager
+resource "aws_iam_role_policy" "ssm_access" {
+  name = "ssm-access"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:UpdateInstanceInformation",
+          "ssmmessages:CreateControlChannel",
+          "ssmmessages:CreateDataChannel",
+          "ssmmessages:OpenControlChannel",
+          "ssmmessages:OpenDataChannel",
+          "s3:GetEncryptionConfiguration"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
