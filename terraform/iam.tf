634867dd4768de82efeb5f7b43b772412271843b
Austin Stevens <geoaustin@gmail.com>
2025-10-26 15:59:45 -0600

    Fix IAM role reference for SSM

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -145,16 +145,10 @@ resource "aws_iam_role_policy_attachment" "ssm_managed_instance" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# SSM Policy for EC2 instance
-resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
 # Additional SSM policy for session manager
 resource "aws_iam_role_policy" "ssm_access" {
   name = "ssm-access"
-  role = aws_iam_role.ec2_role.id
+  role = aws_iam_role.geospatial_platform.id
 
   policy = jsonencode({
     Version = "2012-10-17"
